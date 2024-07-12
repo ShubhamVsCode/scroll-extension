4f139b001748199b71fffed72c1a0a6f22597cfc
shubham <shubhamvscode@gmail.com>
2024-07-12 21:18:46 +0530

    first video

--- content.js
+++ content.js
@@ -3,17 +3,46 @@ const containerSelector =
 
 // Create and style the buttons
 const buttonContainer = document.createElement("div");
+
 buttonContainer.style.position = "fixed";
+
 buttonContainer.style.bottom = "20px";
 buttonContainer.style.right = "20px";
 buttonContainer.style.zIndex = "1000";
+
+const buttonScrollToFirst = document.createElement("button");
+buttonScrollToFirst.innerText = "First Video";
+buttonScrollToFirst.style.margin = "5px";
 
 const buttonScrollToLast = document.createElement("button");
 buttonScrollToLast.innerText = "Last Video";
 buttonScrollToLast.style.margin = "5px";
 
+buttonContainer.appendChild(buttonScrollToFirst);
 buttonContainer.appendChild(buttonScrollToLast);
+
 document.body.appendChild(buttonContainer);
+
+buttonScrollToFirst.addEventListener("click", () => {
+  const containerElement = document.querySelector(containerSelector);
+  if (containerElement) {
+    const draggableItems = containerElement.querySelectorAll("div");
+
+    if (draggableItems.length > 0) {
+      const firstDraggable = draggableItems[0];
+
+      firstDraggable.scrollIntoView({
+        behavior: "instant",
+        block: "center",
+        inline: "center",
+      });
+    } else {
+      console.log("No draggable items found");
+    }
+  } else {
+    console.log("Container element not found");
+  }
+});
 
 buttonScrollToLast.addEventListener("click", () => {
   const containerElement = document.querySelector(containerSelector);
@@ -34,7 +63,4 @@ buttonScrollToLast.addEventListener("click", () => {
   }
 });
 
-// 1. Working on Whiteboard Multiple Instance
-// 2. Adding Whiteboard Homepage APIs
-// 3. And as rumi said about to do this in priority so i will be working on this parralley the new diagram with madeling chat or voice chat
-// So i will be doing rnd over it.
+/
